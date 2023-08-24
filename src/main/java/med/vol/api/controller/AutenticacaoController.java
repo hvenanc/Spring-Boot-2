abed803df899565b76884816c3bda64d3f0e73e5
hvenanc <hvs@poli.br>
2023-08-24 20:32:54 -0300

    Autenticação

---
+++ src/main/java/med/vol/api/controller/AutenticacaoController.java
@@ -0,0 +1,27 @@
+package med.vol.api.controller;
+
+import jakarta.validation.Valid;
+import med.vol.api.entities.dto.usuario.DadosAutenticacao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity<DadosAutenticacao> logar(@RequestBody @Valid DadosAutenticacao dados) {
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var autenticacao = manager.authenticate(token);
+        return ResponseEntity.ok().build();
+    }
+}

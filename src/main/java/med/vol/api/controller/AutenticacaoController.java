27b1d1f9ea9439a5d530e2b459833896a043bd51
hvenanc <hvs@poli.br>
2023-08-26 20:44:09 -0300

    Geração do Token JWT

--- src/main/java/med/vol/api/controller/AutenticacaoController.java
+++ src/main/java/med/vol/api/controller/AutenticacaoController.java
@@ -1,7 +1,10 @@
 package med.vol.api.controller;
 
 import jakarta.validation.Valid;
+import med.vol.api.entities.Usuario;
 import med.vol.api.entities.dto.usuario.DadosAutenticacao;
+import med.vol.api.service.DadosTokenJWT;
+import med.vol.api.service.ManagerTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,10 +21,14 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private ManagerTokenService tokenService;
+
     @PostMapping
-    public ResponseEntity<DadosAutenticacao> logar(@RequestBody @Valid DadosAutenticacao dados) {
+    public ResponseEntity<DadosTokenJWT> logar(@RequestBody @Valid DadosAutenticacao dados) {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var autenticacao = manager.authenticate(token);
-        return ResponseEntity.ok().build();
+        var tokenUser = tokenService.gerarToken((Usuario) autenticacao.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(tokenUser));
     }
 }
---
+++ src/main/java/med/vol/api/service/ManagerTokenService.java
@@ -0,0 +1,38 @@
+package med.vol.api.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.vol.api.entities.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class ManagerTokenService {
+
+    @Value("${api.security.token.secret}")
+    public String secret;
+
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+           throw new RuntimeException("Erro ao gerar o Token", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
